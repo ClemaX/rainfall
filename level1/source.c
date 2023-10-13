dd4b24f1e8778feb28ee51536f35d067cb22d5c6
Clement Hamada <clementhamada@protonmai.com>
2023-10-13 17:22:38 +0200

    level1: Add source

---
+++ level1/source.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void	run(void)
+{
+	fwrite("Good... Wait what?\n",1,0x13,stdout);
+	system("/bin/sh");
+}
+
+int		main()
+{
+	char str [76];
+
+	gets(str);
+}
