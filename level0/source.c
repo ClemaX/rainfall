30e5b661bf6f28f54ba720fd1c1534aba5a6fd30
Clement Hamada <clementhamada@protonmai.com>
2023-10-13 17:32:06 +0200

    level1: source: Format

--- level0/source.c
+++ level0/source.c
@@ -6,28 +6,28 @@
 #include <unistd.h>
 
 int main(int ac,char **av)
-
 {
-  int n;
-  gid_t egid;
-  uid_t euid;
-  char *exec_av [2];
-  
-  n = atoi(av[1]);
-  if (n == 423) {
-    exec_av[0] = strdup("/bin/sh");
-    exec_av[1] = NULL;
+ 	int n;
+	gid_t egid;
+	uid_t euid;
+	char *exec_av [2];
 
-    egid = getegid();
-    euid = geteuid();
+	n = atoi(av[1]);
+	if (n == 423)
+	{
+		exec_av[0] = strdup("/bin/sh");
+		exec_av[1] = NULL;
 
-    setresgid(egid, egid, egid);
-    setresuid(euid, euid, euid);
+		egid = getegid();
+		euid = geteuid();
 
-    execv("/bin/sh", exec_av);
-  }
-  else {
-    fwrite("No !\n", 1, 5, stderr);
-  }
-  return 0;
+		setresgid(egid, egid, egid);
+		setresuid(euid, euid, euid);
+
+		execv("/bin/sh", exec_av);
+	}
+	else
+		fwrite("No !\n", 1, 5, stderr);
+
+	return 0;
 }
